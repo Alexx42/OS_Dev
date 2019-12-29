3088dcbab6c672d1bfdd39bf24d3899a07b22d20
Alexx42 <legoff.alexandre06@gmail.com>
2019-12-29 18:20:43 +0000

    multiboot for the kernel

---
+++ kernel/boot.s
@@ -0,0 +1,34 @@
+; Declare constants for the multiboot header
+MBALIGN equ	1 << 0		; align loaded modules on page boundaries
+MEMINFO equ	1 << 1		; provide memory map
+FLAGS	equ	1 << 2		; multiboot 'flag' field
+MAGIC	equ	0x1BADB002	; magic number
+CHECKSUM	equ	-(MAGIC + FLAGS) ; checksum of above to prove we are MB
+
+; Declare a multiboot header that marks the program as a kernel.
+
+section .multiboot
+align 4
+	dd MAGIC
+	dd FLAGS
+	dd CHECKSUM
+
+section .bss
+align 16
+stack_bottom:
+resb 16384 ;16 KiB
+stack_top:
+
+section .text
+global _start:function (_start.end - _start)
+_start:
+	mov esp, stack_top	;esp register points to the top of the stack	 
+
+extern	kernel_main
+call	kernel_main
+
+	cli			; clear interrupt enable in eflags
+.hang:	hlt			; wait for the next interrupt
+	jmp .hang		; jump to the hlt instructions
+
+.end:
