a1a678de907f01020f08936678552791b6032335
Alexx42 <legoff.alexandre06@gmail.com>
2019-12-29 14:47:32 +0000

    doc

---
+++ bootloader/CString.h
@@ -0,0 +1,10 @@
+#ifndef __CSTRING__
+# define __CSTRING__
+#include "Types.h"
+
+class CString
+{
+public:
+	static byte Strlen(const char * inStrSource);
+};
+# endif
---
+++ bootloader/Types.h
@@ -0,0 +1,11 @@
+#ifndef __TYPES__
+#define __TYPES__
+
+#define true		0x01
+#define false		0x00
+
+typedef unsigned char	byte;
+typedef unsigned short	word;
+typedef unsigned long	dword;
+
+#endif
